04e2066abb3ae717272e566b9ed408e227205069
Jean-Eudes CODO <eudescodo00@gmail.com>
2025-06-18 03:22:26 +0100

    Update sensor_publisher.py

--- Test2/src/sensor_data_evaluation/sensor_data_evaluation/sensor_publisher.py
+++ Test2/src/sensor_data_evaluation/sensor_data_evaluation/sensor_publisher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
+import time
 import random
 
 class SensorPublisher(Node):
@@ -14,13 +15,14 @@ class SensorPublisher(Node):
         timer_period = 0.5  # secondes
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('SensorPublisher initialisé, publication toutes les 0.5s.')
-
+    
     def timer_callback(self):
-        # Générer valeurs aléatoires dans les plages :
+        # Générer valeurs aléatoires dans les plages
         temp = random.uniform(15.0, 35.0)      # °C
         hum = random.uniform(30.0, 70.0)       # %
         pres = random.uniform(950.0, 1050.0)   # hPa
-
+        
+        # Créer et publier le message
         msg = Float32MultiArray()
         msg.data = [float(temp), float(hum), float(pres)]
         self.publisher_.publish(msg)
@@ -37,3 +39,6 @@ def main(args=None):
         node.get_logger().info('SensorPublisher arrêt.')
         node.destroy_node()
         rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
